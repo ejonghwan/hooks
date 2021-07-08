1a11139f95f64f29510a697486b2ddfbccc8ac73
ejonghwan <sun87-1@daum.net>
2021-07-09 00:25:40 +0900

    a

--- src/09_useScroll/Scroll.js
+++ src/09_useScroll/Scroll.js
@@ -1,8 +1,31 @@
-import React, {} from 'react'
+import React, {useEffect, useState} from 'react'
+
+const useScroll = () => {
+    const [state, setState] = useState({
+        x: 0,
+        y: 0,
+    });
+
+    const onScroll = (e) => {
+        setState({y: window.pageYOffset, x: window.scrollX})
+        // console.log(window.scrollX)
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', onScroll)
+        return () => window.addEventListener('scroll', onScroll)
+    }, [])
+    return state
+}
+
 
 const Scroll = () => {
+
+    const { y } = useScroll()
+
     return (
         <div>
+            <h1 style={{ position: "fixed", top:0,  color: y > 200 ? "red" : "blue"}}> use scroll </h1>
             asdasd
         </div>
     )
